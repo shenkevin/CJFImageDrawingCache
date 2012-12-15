697f6de1552be6ba304b3143ed2282225b15a1be
Chris <chris@fothergill.me>
2012-12-15 18:17:34 +0000

    Initial Commit

---
+++ CJFImageDrawingCache/CJFImageDrawingCache.h
@@ -0,0 +1,13 @@
+//
+//  CJFImageDrawingCache.h
+//  CJFImageDrawingCache
+//
+//  Created by Chris on 15/12/2012.
+//  Copyright (c) 2012 Chris Fothergill. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CJFImageDrawingCache : NSObject
+
+@end
---
+++ CJFImageDrawingCache/CJFImageDrawingCache.m
@@ -0,0 +1,13 @@
+//
+//  CJFImageDrawingCache.m
+//  CJFImageDrawingCache
+//
+//  Created by Chris on 15/12/2012.
+//  Copyright (c) 2012 Chris Fothergill. All rights reserved.
+//
+
+#import "CJFImageDrawingCache.h"
+
+@implementation CJFImageDrawingCache
+
+@end
